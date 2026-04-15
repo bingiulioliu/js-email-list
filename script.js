50417b37b05f4cb673bf2913cc52dea2702049ed
Bin Giulio Liu <bingiulio.liu@gmail.com>
2026-04-15 15:54:04 +0200

    CREATE re-fetch btn

--- script.js
+++ script.js
@@ -3,6 +3,7 @@
 const API_URL_RANDOM_MAIL = 'https://flynn.boolean.careers/exercises/api/random/mail';
 
 const mailListHtmlEl = document.querySelector('#list-container');
+const fetchBtnHtmlEl = document.querySelector('#fetch-btn');
 
 for (let i=0; i<10; i++){
     fetch(API_URL_RANDOM_MAIL)
