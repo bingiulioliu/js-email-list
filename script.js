8c9b9fe1cccfe2addc5e743c7a384c8cda66153a
Bin Giulio Liu <bingiulio.liu@gmail.com>
2026-04-15 15:36:57 +0200

    ADD ciclo for per fetch x10

--- script.js
+++ script.js
@@ -4,11 +4,15 @@ const API_URL_RANDOM_MAIL = 'https://flynn.boolean.careers/exercises/api/random/
 
 const mailListHtmlEl = document.querySelector('#list-container');
 
-fetch(API_URL_RANDOM_MAIL)
-    .then((risposta)=>{
-        return risposta.json()
-    })
-    .then ((jsonData)=>{
-        console.log(jsonData);
-        
-    })
+for (let i=0; i<10; i++){
+    fetch(API_URL_RANDOM_MAIL)
+        .then((risposta)=>{
+            return risposta.json()
+        })
+        .then ((jsonData)=>{
+            console.log(jsonData);
+            
+            const mailList = jsonData.result
+
+        })
+    }
