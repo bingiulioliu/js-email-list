f821ba9dacb7be6e5a073399aa70b4c391ccdc34
Bin Giulio Liu <bingiulio.liu@gmail.com>
2026-04-15 16:03:17 +0200

    ADD funzione + event listener TO DO Sostituire le mail invece che aggiungerle

--- script.js
+++ script.js
@@ -16,7 +16,24 @@ for (let i=0; i<10; i++){
             const mail = jsonData.response;
             // Ad ogni ciclo mi aggiunge una mail 
             mailListHtmlEl.innerHTML += `<li> ${mail} </li>`;
-            console.log(mailListHtmlEl);
-            
         })
     }
+console.log(mailListHtmlEl);
+
+function reFetch () {
+for (let i=0; i<10; i++){
+    fetch(API_URL_RANDOM_MAIL)
+        .then((risposta)=>{
+            return risposta.json()
+        })
+        .then ((jsonData)=>{
+            //console.log(jsonData);
+            // Variabile di supporto
+            const mail = jsonData.response;
+            // Ad ogni ciclo mi aggiunge una mail 
+            mailListHtmlEl.innerHTML += `<li> ${mail} </li>`;
+        })
+    }
+}
+
+fetchBtnHtmlEl.addEventListener('click', reFetch);
